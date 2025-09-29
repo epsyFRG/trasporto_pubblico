49ca89054b4cc5cb4682ff30cc36ecda3779d2ae
Emiliano Massari <158219820+epsyFRG@users.noreply.github.com>
2025-09-29 16:03:46 +0200

    upload entities

--- src/main/java/bwgroup4/entities/Mezzi.java
+++ src/main/java/bwgroup4/entities/Mezzi.java
@@ -1,4 +1,104 @@
 package bwgroup4.entities;
 
+import javax.persistence.*;
+import java.util.List;
+
+
+@Entity
+@Table(name = "mezzi")
 public class Mezzi {
+
+    @Id
+    private Long id;
+    @Column(name = "targa_pk", nullable = false)
+    private String targaPK;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "tipo", nullable = false)
+    private TipoMezzi tipo;
+
+    @Column(name = "capienza", nullable = false)
+    private int capienza;
+
+    // relazioni
+
+    @OneToMany(mappedBy = "mezzo")
+    private List<Manutenzioni> manutenzioni;
+
+    @OneToMany(mappedBy = "mezzo")
+    private List<Corse> corse;
+
+    // costruttori
+
+    public Mezzi() {
+    }
+
+    public Mezzi(String targaPK, TipoMezzi tipo, int capienza) {
+        this.targaPK = targaPK;
+        this.tipo = tipo;
+        this.capienza = capienza;
+    }
+
+    // getter e setter per relazioni
+
+    public List<Manutenzioni> getManutenzioni() {
+        return manutenzioni;
+    }
+
+    public void setManutenzioni(List<Manutenzioni> manutenzioni) {
+        this.manutenzioni = manutenzioni;
+    }
+
+    public List<Corse> getCorse() {
+        return corse;
+    }
+
+    public void setCorse(List<Corse> corse) {
+        this.corse = corse;
+    }
+
+    // getter e setter
+
+    public String getTargaPK() {
+        return targaPK;
+    }
+
+    public void setTargaPK(String targaPK) {
+        this.targaPK = targaPK;
+    }
+
+    public TipoMezzi getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(TipoMezzi tipo) {
+        this.tipo = tipo;
+    }
+
+    public int getCapienza() {
+        return capienza;
+    }
+
+    public void setCapienza(int capienza) {
+        this.capienza = capienza;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Mezzi{" +
+                "targaPK=" + targaPK +
+                ", tipo=" + tipo +
+                ", capienza=" + capienza +
+                '}';
+    }
+
+
 }

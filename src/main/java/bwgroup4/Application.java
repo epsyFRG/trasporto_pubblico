e7273f8e9d1c348f711f6eff3d0e6894112f82e5
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-09-29 15:59:23 +0200

    commit1

--- src/main/java/bwgroup4/Application.java
+++ src/main/java/bwgroup4/Application.java
@@ -1,8 +1,20 @@
 package bwgroup4;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+    static EntityManagerFactory emf= Persistence.createEntityManagerFactory("trasportopubblico");
     public static void main(String[] args) {
+
+        EntityManager em=emf.createEntityManager();
+
+        em.close();
+        emf.close();
+
+
         System.out.println("Hello World!");
     }
 }

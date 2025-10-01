cd829c7e78e51a9c10da0e131c65d9d1537bfdab
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-10-01 22:05:18 +0200

    commit

--- src/main/java/bwgroup4/Application.java
+++ src/main/java/bwgroup4/Application.java
@@ -155,90 +155,11 @@ public class Application {
     //1) metodo getPerPeriodoAndEmitt su abbonamenti e biglietti (già fatto, da mettere nello switch) FATTO
     //2) verifica abbonamento in base a tessera (già fatto, da mettere nello switch) FATTO
     //3) metodo getmanutenzioni per mezzo (già fatto, da mettere nello switch) FATTO
-    //4) metodo getVidimazioni per mezzo per periodo tempo (già fatto, da mettere nello switch) FATTO 
+    //4) metodo getVidimazioni per mezzo per periodo tempo (già fatto, da mettere nello switch) FATTO
     //5) metodo inserisco id tratta e id mezzo e ottengo media tempo effettivo FATTO
     //6) assegnare mezzo a tratta, cioè creare una nuova corsa FATTO
 
     public static void main(String[] args) {
-
-
-//        Tratta tratta1 = new Tratta("Beverino", "Ceparana", 20);
-//        tDao.save(tratta1);
-//
-//        Mezzi mezzo1 = new Mezzi("CIAO", TipoMezzi.AUTOBUS, 30);
-//        mDao.save(mezzo1);
-//
-//        Corse corsa1 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 1, 8, 30),
-//                LocalDateTime.of(2025, 10, 1, 9, 15)
-//        );
-//        cDao.save(corsa1);
-//        Corse corsa2 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 1, 10, 0),
-//                LocalDateTime.of(2025, 10, 1, 10, 25)
-//        );
-//        cDao.save(corsa2);
-//        Corse corsa3 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 1, 14, 15),
-//                LocalDateTime.of(2025, 10, 1, 14, 35)
-//        );
-//        cDao.save(corsa3);
-//        Corse corsa4 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 2, 7, 0),
-//                LocalDateTime.of(2025, 10, 2, 8, 10)
-//        );
-//        cDao.save(corsa4);
-//        Corse corsa5 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 2, 16, 45),
-//                LocalDateTime.of(2025, 10, 2, 17, 30)
-//        );
-//        cDao.save(corsa5);
-//        Corse corsa6 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 3, 9, 20),
-//                LocalDateTime.of(2025, 10, 3, 10, 0)
-//        );
-//        cDao.save(corsa6);
-//        Corse corsa7 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 3, 11, 30),
-//                LocalDateTime.of(2025, 10, 3, 12, 45)
-//        );
-//        cDao.save(corsa7);
-//        Corse corsa8 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 4, 8, 0),
-//                LocalDateTime.of(2025, 10, 4, 8, 40)
-//        );
-//        cDao.save(corsa8);
-//        Corse corsa9 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 4, 13, 15),
-//                LocalDateTime.of(2025, 10, 4, 14, 5)
-//        );
-//        cDao.save(corsa9);
-//        Corse corsa10 = new Corse(
-//                mezzo1,
-//                tratta1,
-//                LocalDateTime.of(2025, 10, 5, 6, 45),
-//                LocalDateTime.of(2025, 10, 5, 7, 15)
-//        );
-//        cDao.save(corsa10);
-
 
         Scanner scanner = new Scanner(System.in);
         LocalDate today=LocalDate.now();

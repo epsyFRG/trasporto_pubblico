a8af4f52dc464608b3a3cc1730819193ae18445e
GiuseppeTuccilli <giuseppetuccilli@gmail.com>
2025-10-01 14:47:17 +0200

    com5

--- src/main/java/bwgroup4/Application.java
+++ src/main/java/bwgroup4/Application.java
@@ -138,6 +138,19 @@ public class Application {
 
     }
 
+    //1) metodo getIsAdmin in base a id persona
+    //-------------------------------------------
+    //utente
+    //1) metodo nuovo abbonamento passando id venditore
+    //2) metodo nuovo biglietto pasando id venditore
+    //3) metodo setScadenza su tessera se dataScadenza > oggi
+    //4) nuova vidimazione fornendo codice biglietto e id mezzo
+    //---------------------------------
+    //admin
+    //1) metodo getPerPeriodoAndEmitt su abbonamenti e biglietti (già fatto, da mettere nello switch)
+    //2) verifica abbonamento in mase a tessera (già fatto, da mettere nello switch)
+    //3) metodo getmanutenzioni per mezzo (già fatto, da mettere nello switch)
+    //4) metodo getVidimazioni per mezzo per periodo tempo (già fatto, da mettere nello switch)
 
     public static void main(String[] args) {
 
